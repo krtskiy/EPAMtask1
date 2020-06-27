d605037b1ccf76a43beaa0b6150295c6d41ba490
jMushmyH <jmushmyh@email.com>
2020-06-27 14:11:58 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Part2.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part2.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part2.java
@@ -3,7 +3,10 @@ package com.epam.rd.java.basic.practice1;
 public class Part2 {
 
     public static void main(String[] args) {
-
+        int result = 0;
+        for (int i = 0; i < args.length; i++) {
+            result += Integer.valueOf(args[i]);
+        }
+        System.out.print(result);
     }
-	
 }
