80bcab09d393afd962625ef407d383bf2eeb5a8a
jMushmyH <jmushmyh@email.com>
2020-06-27 13:18:12 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Part3.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part3.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part3.java
@@ -2,8 +2,11 @@ package com.epam.rd.java.basic.practice1;
 
 public class Part3 {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] params) {
+                      if (params.length > 0) {
+                    params[params.length-1] = params[params.length-1].trim();
+        }
+        System.out.print(String.join(" ", params));
     }
-	
+
 }
