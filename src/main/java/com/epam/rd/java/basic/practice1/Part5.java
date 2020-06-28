4f081df723f646525cfda6eb3a54166ddb253b0f
jMushmyH <jmushmyh@email.com>
2020-06-28 22:29:53 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Part5.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part5.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part5.java
@@ -3,7 +3,13 @@ package com.epam.rd.java.basic.practice1;
 public class Part5 {
 
     public static void main(String[] args) {
-        
+        int argsInt = Integer.valueOf(args[0]);
+        int result = 0;
+        while (argsInt != 0) {
+            result += argsInt % 10;
+            argsInt /= 10;
+        }
+        System.out.print(result);
     }
 	
 }
