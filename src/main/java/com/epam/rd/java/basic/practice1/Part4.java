ea3365377ad705a8c00728bd85d8c2632594ba0d
jMushmyH <jmushmyh@email.com>
2020-06-28 21:44:12 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Part4.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part4.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part4.java
@@ -3,7 +3,17 @@ package com.epam.rd.java.basic.practice1;
 public class Part4 {
 
     public static void main(String[] args) {
-        
+        int firstNumber = Integer.valueOf(args[0]);
+        int secondNumber = Integer.valueOf(args[1]);
+        while (firstNumber != 0 && secondNumber != 0) {
+            if (firstNumber > secondNumber) {
+                firstNumber %= secondNumber;
+            }
+            else {
+                secondNumber %= firstNumber;
+            }
+        }
+        System.out.println(secondNumber + firstNumber);
     }
 
 }
