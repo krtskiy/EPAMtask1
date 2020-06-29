f004e9cd2adf01f36ab54a91013225cffd6daf0e
jMushmyH <jmushmyh@email.com>
2020-06-29 20:49:07 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Demo.java

--- src/main/java/com/epam/rd/java/basic/practice1/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Demo.java
@@ -4,7 +4,31 @@ package com.epam.rd.java.basic.practice1;
 public class Demo {
 
     public static void main(String[] args) {
-        
+        Part1.main(new String[]{});
+
+        System.out.println("\n~~~~~~~~~~");
+
+        Part2.main(new String[]{"17", "22", "3"});
+
+        System.out.println("\n~~~~~~~~~~");
+
+        Part3.main(new String[]{"4", "2", "0", "endWithoutSpaces"});
+
+        System.out.println("\n~~~~~~~~~~");
+
+        Part4.main(new String[]{"30", "12"});
+
+        System.out.println("\n~~~~~~~~~~");
+
+        Part5.main(new String[]{"6528"});
+
+        System.out.println("\n~~~~~~~~~~");
+
+        Part6.main(new String[]{"9"});
+
+        System.out.println("\n~~~~~~~~~~");
+
+        Part7.main(new String[]{});
     }
 
 }
