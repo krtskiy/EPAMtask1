0d0d33fb0cc5b91e23a8b49a223f11551665a04c
jMushmyH <jmushmyh@email.com>
2020-06-29 11:10:37 +0000

    Update src/main/java/com/epam/rd/java/basic/practice1/Part6.java

--- src/main/java/com/epam/rd/java/basic/practice1/Part6.java
+++ src/main/java/com/epam/rd/java/basic/practice1/Part6.java
@@ -1,9 +1,32 @@
 package com.epam.rd.java.basic.practice1;
 
+import java.util.Arrays;
+
 public class Part6 {
 
     public static void main(String[] args) {
-        
+        int indeks = Integer.valueOf(args[0]);
+        int[] massivProstihChisel = new int[indeks];
+        int testChislo = 2;
+        int kudaStavimChislo = 0;
+        boolean isSimple;
+        while (kudaStavimChislo < indeks) {
+            int i = 0;
+            isSimple = true;
+            while (isSimple && i < kudaStavimChislo) {
+                if (testChislo % massivProstihChisel[i] == 0) {
+                    isSimple = false;
+                }
+                i++;
+            }
+            if (isSimple) {
+                massivProstihChisel[kudaStavimChislo] = testChislo;
+                kudaStavimChislo++;
+                testChislo++;
+            } else {
+                testChislo++;
+            }
+        }
     }
-	
+
 }
